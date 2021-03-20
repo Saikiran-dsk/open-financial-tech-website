8a96a877ae435bd0531eb1fdb4fed45cbc627980
Saikiran <duddukurisaikiran@gmail.com>
2021-03-21 02:01:31 +0530

    'changes'

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen( 8080);
 
 
