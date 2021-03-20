1f6a4c52e9ce2cb6ce85582e6ceefd0331b6f92b
Saikiran <duddukurisaikiran@gmail.com>
2021-03-21 02:50:47 +0530

    changes

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const path = require('path');
  
 const app = express();
-app.use(express.static(__dirname+'/open-financial-tech-website/dist/'));
+app.use(express.static(__dirname+'/dist/open-financial-tech-website'));
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 // app.use(express.static(__dirname + '/dist/'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/open-financial-tech-website/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/open-financial-tech-website/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen( process.env.PORT || 8080);
